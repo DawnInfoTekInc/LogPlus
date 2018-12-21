da6f4e9d7be760039295dbd99148931ee96bdf06
ryanwang <aqahc.support@dawninfotek.com>
2018-12-21 12:43:36 -0500

    logback json format hard coded version

--- src/main/java/com/dawninfotek/logx/extension/logback/LogXJsonLayout.java
+++ src/main/java/com/dawninfotek/logx/extension/logback/LogXJsonLayout.java
@@ -9,32 +9,47 @@ public class LogXJsonLayout extends JsonLayout {
 
 	protected Map toJsonMap(ILoggingEvent event) {
 
-         Map<String, Object> map = super.toJsonMap(event);
-         // test
-         map.remove("mdc");
-         map.remove("context");
-         add("appName", true, getApplicationName(event), map);
-         add("serviceName", true, MDC.get("transactionPath"), map);
-         add("hostname", true, MDC.get("hostName"), map);
-         add("processId", true, MDC.get("processId"), map);
-         add("corrId", true, MDC.get("uuid"), map);
-         add("sessionId", true, MDC.get("sessionId"), map);
-         add("userName", true, MDC.get("userName"), map);
-         add("logMethod", true, getMethod(event), map);
-         return map;
-     }
+        Map<String, Object> map = super.toJsonMap(event);
+        map.remove("mdc");
+        map.remove("context");
+        add("logLevel", true, map.get("level").toString(), map);
+        map.remove("level");
+        add("appName", true, getApplicationName(event), map);
+        add("serviceName", true, getFromMDC("transactionPath"), map);
+        add("remoteIp", true, getFromMDC("remoteIp"), map);
+        add("hostname", true, getFromMDC("hostName"), map);
+        add("processId", true, getFromMDC("processId"), map);
+        add("corrId", true, getFromMDC("uuid"), map);
+        add("sessionId", true, getFromMDC("sessionId"), map);
+        add("clientId", true, getFromMDC("userName"), map);
+        add("logMethod", true, getMethod(event), map);
+        return map;
+    }
 
 	protected String getApplicationName(ILoggingEvent event) {
 		String applicationName = MDC.get("applicationName");
 		if(applicationName != null) {
 			return applicationName;
 		}
-		return event.getLoggerContextVO().getName();
+		applicationName = event.getLoggerContextVO().getName();
+		if(applicationName.equals("default")) {
+			return "";
+		}
+		return applicationName;
 	}
 
 	protected String getMethod(ILoggingEvent event) {
 		String[] classMethods = event.getLoggerName().split("\\.");
 		return classMethods[classMethods.length - 1];
 	}
+
+	protected String getFromMDC(String value) {
+		String result = MDC.get(value);
+		if(result == null) {
+			return "";
+		}
+		return result;
+	}
+
 
 }
